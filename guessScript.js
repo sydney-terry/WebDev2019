1c0ceb6be35206337720a333bfda21f5a097ebd6
sydney terry <sydneysue2242@gmail.com>
2019-07-06 21:29:43 -0600

    fixed my mistakes

--- guessScript.js
+++
@@ -1,7 +0,0 @@
-let randomNum = Math.floor(Math.random() * Math.floor(99));
-
-makeGuess = (form) => {
-    let guessedNum = parseInnt(form.inbox.value, 10);
-    form.inputbox.value = "";
-    document.getElementById("response").innerHTML = "You guessed " + guessedNum
-}
